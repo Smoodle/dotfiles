01521d91d0895fbc21d840f000add945c19c0c45
Smoodle <migwwe@gmail.com>
2020-12-10 18:02:26 +0000

    Added new scripst to change tablet size

---
+++ scripts/.scripts/tablet/map_to_output.sh
@@ -0,0 +1,1 @@
+xsetwacom --set "HUION Huion Tablet stylus" MapToOutput HEAD-0
---
+++ scripts/.scripts/tablet/reset_wacom.sh
@@ -0,0 +1,2 @@
+xsetwacom set "HUION Huion Tablet stylus" "Area" "0 0 24384 15240"
+xsetwacom set "HUION Huion Tablet stylus" "MapToOutput" "desktop"
