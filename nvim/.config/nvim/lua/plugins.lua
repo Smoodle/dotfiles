a22630c5a4333770cffe28c1cacb950f0bb35d85
Smoodle <migwwe@gmail.com>
2022-03-12 13:34:47 +0000

    New snippet engine

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -5,15 +5,6 @@ packer.init()
 
 use {'wbthomason/packer.nvim', opt = true}
 
---use {'navarasu/onedark.nvim',
---	config = function()
---		require('onedark').setup {
---			style = 'darker'
---		}
---		require('onedark').load()
---	end
---}
-
 use {"rebelot/kanagawa.nvim",
 	config = function()
 		require('kanagawa').setup({
@@ -32,7 +23,7 @@ use {"rebelot/kanagawa.nvim",
 			overrides = {},
 		})
 
--- setup must be called before loading
+		-- setup must be called before loading
 		vim.cmd("colorscheme kanagawa")
 	end
 }
@@ -50,22 +41,20 @@ use { "beauwilliams/focus.nvim", config = function() require("focus").setup() en
 use {'junegunn/fzf', dir = '~/.fzf', run = './install --all' }
 use {'junegunn/fzf.vim'}
 
-use 'saadparwaiz1/cmp_luasnip'
-
-use {'SirVer/ultisnips',
-	config = function ()
-		vim.g.UltiSnipsSnippetDirectories = { '~/.config/nvim/Snippets' }
+use {'L3MON4D3/LuaSnip',
+	config = function()
+		require("luasnip.loaders.from_snipmate").lazy_load()
 	end
 }
 
-use {'quangnguyen30192/cmp-nvim-ultisnips'}
+use 'saadparwaiz1/cmp_luasnip'
 
 use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons' },
-  config = function()
-	  require("evilline")
-  end
+	'nvim-lualine/lualine.nvim',
+	requires = { 'kyazdani42/nvim-web-devicons' },
+	config = function()
+		require("evilline")
+	end
 }
 
 use { 'romgrk/barbar.nvim',
@@ -127,20 +116,20 @@ use {'glepnir/dashboard-nvim',
 		vim.g.dashboard_default_executive = "telescope"
 		vim.g.dashboard_custom_header = {
 			"                                   ",
-		"                                   ",
-		"                                   ",
-		"   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-		"    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-		"          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-		"           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-		"          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-		"   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-		"  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-		" ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-		" ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-		"      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-		"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-		"                                   ",
+			"                                   ",
+			"                                   ",
+			"   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+			"    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+			"          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+			"           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+			"          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+			"   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+			"  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+			" ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+			" ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+			"      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+			"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+			"                                   ",
 		}
 
 		vim.g.dashboard_custom_section = {
@@ -158,10 +147,10 @@ use {'glepnir/dashboard-nvim',
 }
 
 use {
-    'numToStr/Comment.nvim',
-    config = function()
-        require('Comment').setup()
-    end
+	'numToStr/Comment.nvim',
+	config = function()
+		require('Comment').setup()
+	end
 }
 
 use {
@@ -172,13 +161,13 @@ use {
 		require('telescope').setup{
 			extensions = {
 				fzf = {
-	  				fuzzy = true,                    -- false will only do exact matching
-	  				override_generic_sorter = true,  -- override the generic sorter
-	  				override_file_sorter = true,     -- override the file sorter
-	  				case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-									   -- the default case_mode is "smart_case"
+					fuzzy = true,                    -- false will only do exact matching
+					override_generic_sorter = true,  -- override the generic sorter
+					override_file_sorter = true,     -- override the file sorter
+					case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
 				}
-  			},
+			},
 			defaults = {
 				mappings = {
 					i = {
@@ -215,78 +204,66 @@ use {
 	'williamboman/nvim-lsp-installer',
 }
 
---[[
-use {'RishabhRD/nvim-lsputils',
-	requires = {'RishabhRD/popfix'},
-	config = function()
-		vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
-		vim.lsp.handlers['textDocument/references'] = require'lsputil.locations'.references_handler
-		vim.lsp.handlers['textDocument/definition'] = require'lsputil.locations'.definition_handler
-		vim.lsp.handlers['textDocument/declaration'] = require'lsputil.locations'.declaration_handler
-		vim.lsp.handlers['textDocument/typeDefinition'] = require'lsputil.locations'.typeDefinition_handler
-		vim.lsp.handlers['textDocument/implementation'] = require'lsputil.locations'.implementation_handler
-		vim.lsp.handlers['textDocument/documentSymbol'] = require'lsputil.symbols'.document_handler
-		vim.lsp.handlers['workspace/symbol'] = require'lsputil.symbols'.workspace_handler
-	end} ]]
-
 use { 'tami5/lspsaga.nvim',
 	config = function ()
 		local lspsaga = require 'lspsaga'
-lspsaga.setup { -- defaults ...
-  debug = false,
-  use_saga_diagnostic_sign = true,
-  -- diagnostic sign
-  error_sign = "",
-  warn_sign = "",
-  hint_sign = "",
-  infor_sign = "",
-  diagnostic_header_icon = "   ",
-  -- code action title icon
-  code_action_icon = " ",
-  code_action_prompt = {
-	enable = true,
-	sign = true,
-	sign_priority = 40,
-	virtual_text = true,
-  },
-  finder_definition_icon = "  ",
-  finder_reference_icon = "  ",
-  max_preview_lines = 10,
-  finder_action_keys = {
-	open = "o",
-	vsplit = "s",
-	split = "i",
-	quit = "q",
-	scroll_down = "<C-f>",
-	scroll_up = "<C-b>",
-  },
-  code_action_keys = {
-	quit = "q",
-	exec = "<CR>",
-  },
-  rename_action_keys = {
-	quit = "<C-c>",
-	exec = "<CR>",
-  },
-  definition_preview_icon = "  ",
-  border_style = "single",
-  rename_prompt_prefix = "➤",
-  server_filetype_map = {},
-  diagnostic_prefix_format = "%d. ",
-}
+		lspsaga.setup { -- defaults ...
+			debug = false,
+			use_saga_diagnostic_sign = true,
+			-- diagnostic sign
+			error_sign = "",
+			warn_sign = "",
+			hint_sign = "",
+			infor_sign = "",
+			diagnostic_header_icon = "   ",
+			-- code action title icon
+			code_action_icon = " ",
+			code_action_prompt = {
+				enable = true,
+				sign = true,
+				sign_priority = 40,
+				virtual_text = true,
+			},
+			finder_definition_icon = "  ",
+			finder_reference_icon = "  ",
+			max_preview_lines = 10,
+			finder_action_keys = {
+				open = "o",
+				vsplit = "s",
+				split = "i",
+				quit = "q",
+				scroll_down = "<C-f>",
+				scroll_up = "<C-b>",
+			},
+			code_action_keys = {
+				quit = "q",
+				exec = "<CR>",
+			},
+			rename_action_keys = {
+				quit = "<C-c>",
+				exec = "<CR>",
+			},
+			definition_preview_icon = "  ",
+			border_style = "single",
+			rename_prompt_prefix = "➤",
+			server_filetype_map = {},
+			diagnostic_prefix_format = "%d. ",
+		}
 	end }
-
---use {'kosayoda/nvim-lightbulb',
---	config = function()
---		vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
---	end}
 
 use { 'hrsh7th/nvim-cmp',
 	requires = {'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', 'onsails/lspkind-nvim'},
 	config = function()
 		vim.cmd [[ set completeopt=menu,menuone,noselect ]]
 		-- Setup nvim-cmp.
+
+		local has_words_before = function()
+			local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+			return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+		end
+
 		local cmp = require'cmp'
+		local luasnip = require("luasnip")
 		local lspkind = require('lspkind')
 
 		cmp.setup({
@@ -294,9 +271,9 @@ use { 'hrsh7th/nvim-cmp',
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
 					--vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					--require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-					 vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+					--vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 				end,
 			},
 			formatting = {
@@ -322,30 +299,39 @@ use { 'hrsh7th/nvim-cmp',
 					c = cmp.mapping.close(),
 				}),
 				['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				['<Tab>'] = function(fallback)
+				["<Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif has_words_before() then
+						cmp.complete()
 					else
 						fallback()
 					end
-				end,
-				['<S-Tab>'] = function(fallback)
+				end, { "i", "s" }),
+
+				["<S-Tab>"] = cmp.mapping(function(fallback)
 					if cmp.visible() then
 						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
 					else
 						fallback()
 					end
-				end,
+				end, { "i", "s" }),
 			},
+
 			sources = cmp.config.sources({
 				{ name = 'nvim_lsp' },
 				--{ name = 'vsnip' }, -- For vsnip users.
-				--{ name = 'luasnip' }, -- For luasnip users.
-				{ name = 'ultisnips' }, -- For ultisnips users.
+				{ name = 'luasnip' }, -- For luasnip users.
+				--{ name = 'ultisnips' }, -- For ultisnips users.
 				-- { name = 'snippy' }, -- For snippy users.
-			}, {
+				{ name = 'path' },
+				}, {
 					{ name = 'buffer' },
-				})
+			})
 		})
 
 		-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
@@ -359,9 +345,9 @@ use { 'hrsh7th/nvim-cmp',
 		cmp.setup.cmdline(':', {
 			sources = cmp.config.sources({
 				{ name = 'path' }
-			}, {
+				}, {
 					{ name = 'cmdline' }
-				})
+			})
 		})
 	end
 }
