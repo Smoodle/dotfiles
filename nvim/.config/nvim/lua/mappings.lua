a64110f364b1216e093fc35bf900ba3125d6a5c2
Smoodle <migwwe@gmail.com>
2022-07-26 04:43:51 +0100

    Added custom command to log a variable

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -50,3 +50,26 @@ map('n', '<leader>rg', ':Telescope grep_string search="" only_sort_text=true<cr>
 map('n', '<leader>gh', ':Lspsaga lsp_finder<CR>', opt)
 map('n', '<leader>ca', ':Lspsaga code_action<CR>', opt)
 map('n', 'K', ':Lspsaga hover_doc<CR>', opt)
+
+local function get_logger_message(word, pos)
+	local type = vim.bo.filetype
+	local path = vim.api.nvim_buf_get_name(0)
+
+	if type == "lua" then
+		return "print(\"  " .. path .. " Line " .. pos[1] .. " Col " .. pos[2] .. "  \" .. " .. word .. ")"
+	elseif type == "javascript" or type == "typescript" then
+		return "console.log(`  " .. path .. " Line " .. pos[1] .. " Col " .. pos[2] .. "  ${" .. word .. "}`);"
+	end
+end
+
+function Log_at_point()
+	local selected_word = vim.fn.expand("<cword>")
+
+	vim.api.nvim_command("normal $%")
+	local pos = vim.api.nvim_win_get_cursor(0)
+
+	vim.api.nvim_buf_set_text(0, pos[1], 0, pos[1], 0, { get_logger_message(selected_word, pos), "" })
+	vim.api.nvim_command("normal =j")
+end
+
+map('n', '<leader>p', ':lua Log_at_point()<CR>', opt)
