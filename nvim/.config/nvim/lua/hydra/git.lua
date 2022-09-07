de2168056360c95f2347a50972186c941dbe00ee
Smoodle <migwwe@gmail.com>
2022-09-07 23:45:01 +0100

    Changed neovim plugins and settings

--- nvim/.config/nvim/lua/hydra/git.lua
+++ nvim/.config/nvim/lua/hydra/git.lua
@@ -10,47 +10,47 @@ local hint = [[
 ]]
 
 Hydra({
-   hint = hint,
-   config = {
-      color = 'pink',
-      invoke_on_body = true,
-      hint = {
-         position = 'bottom',
-         border = 'rounded'
-      },
-      on_enter = function()
-         vim.bo.modifiable = false
-         gitsigns.toggle_signs(true)
-         gitsigns.toggle_linehl(true)
-      end,
-      on_exit = function()
-         gitsigns.toggle_signs(false)
-         gitsigns.toggle_linehl(false)
-         gitsigns.toggle_deleted(false)
-      end
-   },
-   mode = {'n','x'},
-   body = '<leader>g',
-   heads = {
-      { 'J', function()
-            if vim.wo.diff then return ']c' end
-            vim.schedule(function() gitsigns.next_hunk() end)
-            return '<Ignore>'
-         end, { expr = true } },
-      { 'K', function()
-            if vim.wo.diff then return '[c' end
-            vim.schedule(function() gitsigns.prev_hunk() end)
-            return '<Ignore>'
-         end, { expr = true } },
-      { 's', ':Gitsigns stage_hunk<CR>', { silent = true } },
-      { 'u', gitsigns.undo_stage_hunk },
-      { 'S', gitsigns.stage_buffer },
-      { 'p', gitsigns.preview_hunk },
-      { 'd', gitsigns.toggle_deleted, { nowait = true } },
-      { 'b', gitsigns.blame_line },
-      { 'B', function() gitsigns.blame_line{ full = true } end },
-      { '/', gitsigns.show, { exit = true } }, -- show the base of the file
-      { '<Enter>', '<cmd>Neogit<CR>', { exit = true } },
-      { 'q', nil, { exit = true, nowait = true } },
-   }
+	hint = hint,
+	config = {
+		color = 'pink',
+		invoke_on_body = true,
+		hint = {
+			position = 'bottom',
+			border = 'rounded'
+		},
+		on_enter = function()
+			vim.bo.modifiable = false
+			gitsigns.toggle_signs(true)
+			gitsigns.toggle_linehl(true)
+		end,
+		on_exit = function()
+			gitsigns.toggle_signs(false)
+			gitsigns.toggle_linehl(false)
+			gitsigns.toggle_deleted(false)
+		end
+	},
+	mode = { 'n', 'x' },
+	body = '<leader>g',
+	heads = {
+		{ 'J', function()
+			if vim.wo.diff then return ']c' end
+			vim.schedule(function() gitsigns.next_hunk() end)
+			return '<Ignore>'
+		end, { expr = true } },
+		{ 'K', function()
+			if vim.wo.diff then return '[c' end
+			vim.schedule(function() gitsigns.prev_hunk() end)
+			return '<Ignore>'
+		end, { expr = true } },
+		{ 's', ':Gitsigns stage_hunk<CR>', { silent = true } },
+		{ 'u', gitsigns.undo_stage_hunk },
+		{ 'S', gitsigns.stage_buffer },
+		{ 'p', gitsigns.preview_hunk },
+		{ 'd', gitsigns.toggle_deleted, { nowait = true } },
+		{ 'b', gitsigns.blame_line },
+		{ 'B', function() gitsigns.blame_line { full = true } end },
+		{ '/', gitsigns.show, { exit = true } }, -- show the base of the file
+		{ '<Enter>', '<cmd>Neogit<CR>', { exit = true } },
+		{ 'q', nil, { exit = true, nowait = true } },
+	}
 })
