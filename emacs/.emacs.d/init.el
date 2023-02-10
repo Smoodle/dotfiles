8a7d3a3642ef11fe5d800a2922bfd33b8796522f
Smoodle <migwwe@gmail.com>
2023-02-10 22:58:01 +0000

    Changed emacs config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,43 +1,98 @@
-(setq gc-cons-threshold (* 1024 1024 500)) ;; 500mb
-(setq read-process-output-max (* 1024 1024 5)) ;; 5mb
+;;; init.el
+
+;; We up the gc threshold to temporarily prevent it from running, then reset it later by
+;; enabling `gcmh-mode'. Not resetting it will cause stuttering/freezes.
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; https://www.masteringemacs.org/article/speed-up-emacs-libjansson-native-elisp-compilation
+(if (and (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+    (setq comp-deferred-compilation t
+          package-native-compile t)
+  (message "Native complation is *not* available, lsp performance will suffer..."))
+
+(unless (functionp 'json-serialize)
+  (message "Native JSON is *not* available, lsp performance will suffer..."))
+
+;; Increase the amount of data which Emacs reads from the process. 
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
+
+;; Do not steal focus while doing async compilations.
+(setq warning-suppress-types '((comp)))
+
 (setq comp-deferred-compilation t)
 
-(setenv "LSP_USE_PLISTS" "true")
-(setq lsp-use-plists t)
+(defvar startup/file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
 
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
-(load custom-file 'noerror 'nomessage)
+(defun startup/revert-file-name-handler-alist ()
+  (setq file-name-handler-alist startup/file-name-handler-alist))
 
-(require 'package)
+(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
 
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
+(setq straight-check-for-modifications '(check-on-save find-when-checking))
+;; Setup straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
+      (bootstrap-version 6))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-(setq package-enable-at-startup nil)
 
+;; Install use-package using straight.el
 (straight-use-package 'use-package)
+
+;; Makes :straight t by default
+(defvar straight-use-package-by-default)
 (setq straight-use-package-by-default t)
+(setq straight-fix-flycheck t)
 
-(use-package org
-  :ensure org-contrib)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+(require 'package)
 
-(garbage-collect)
+;; GCMH - the Garbage Collector Magic Hack
+(use-package gcmh
+  :diminish
+  :custom
+  (gcmh-idle-delay 1000)
+  (gcmh-high-cons-threshold (* 16 1024 1024)) ;; 16 MB
+  :hook (after-init . gcmh-mode))
+
+(setq-default shell-file-name "/bin/zsh")
+
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "firefox")
+
+(straight-use-package 'org)
+
+;; Add local packages
+(add-to-list 'load-path "~/.emacs.d/local")
+
+;; Loads config
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/.emacs.d/config.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.3))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.2))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
