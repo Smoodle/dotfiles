9c81b124ebe2f3f1cdb6fa19b0ae00381584d332
Smoodle <migwwe@gmail.com>
2023-05-22 02:01:35 +0100

    Changed emacs config to org file

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -15,174 +15,18 @@
 
 (straight-use-package 'use-package)
 
+;; Better garbage collection
+(use-package gcmh
+  :init
+  (gcmh-mode 1))
+
 ;; Fix path
 (use-package exec-path-from-shell
-  :ensure t
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 
-;; Customization
-(set-face-attribute 'default nil :height 140)
-(electric-pair-mode t)
-
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-(menu-bar-mode 0)
-(scroll-bar-mode 0)
-(tool-bar-mode 0)
-(setq use-dialog-box nil)
-
-(global-auto-revert-mode 1)
-(setq global-auto-revert-non-file-buffers t)
-
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode)
-
-(setq history-length 25)
-(savehist-mode 1)
-
-(save-place-mode t)
-
-(setq save-place-file (concat user-emacs-directory "places"))
-(setq backup-directory-alist
-      `(("." . ,(concat user-emacs-directory "backups"))))
-
-(setq custom-file (locate-user-emacs-file "custom-vars.el"))
-(load custom-file 'noerror 'nomessage)
-
-(setq-default indent-tabs-mode t)
-(setq tab-width 4)
-
-(global-set-key (kbd "C-s") 'isearch-forward-regexp)
-(global-set-key (kbd "C-r") 'isearch-backward-regexp)
-
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-					 
-;; Dashboard
-(defun my/dashboard-banner ()
-  """Set a dashboard banner including information on package initialization
-   time and garbage collections."""
-  (setq dashboard-banner-logo-title
-        (format "Emacs ready in %.2f seconds with %d garbage collections."
-                (float-time (time-subtract after-init-time before-init-time)) gcs-done)))
-
-(use-package dashboard
-  :init
-  (add-hook 'after-init-hook 'dashboard-refresh-buffer)
-  (add-hook 'dashboard-mode-hook 'my/dashboard-banner)
-  (setq dashboard-center-content t)
-  :config
-  (setq dashboard-startup-banner (concat user-emacs-directory "gura.png"))
-  (dashboard-setup-startup-hook))
-
-;; Theme
-(use-package all-the-icons)
-(use-package doom-themes
-  :ensure t
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
-
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  (doom-themes-treemacs-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
-
-;; Completion
-(setq completion-cycle-threshold 2)
-(setq tab-always-indent 'complete)
-
-;; completion
-
-(use-package orderless
-  :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless partial-completion basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles . (partial-completion))))))
-
-(use-package corfu
-  ;; Optional customizations
-  :custom
-  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                 ;; Enable auto completion
-  ;; (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  (corfu-scroll-margin 5)        ;; Use scroll margin
-  :init
-  (global-corfu-mode))
-
-(use-package kind-icon
-  :ensure t
-  :after corfu
-  :custom
-  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
-  :config
-  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
-
-;; Better ido
-
-(setq ido-create-new-buffer 'always)
-(setq ido-enable-flex-matching t)
-(setq ido-save-directory-list-file nil)
-(setq ido-everywhere t)
-(ido-mode 1)
-
-(use-package ido-completing-read+
-  :config
-  (ido-ubiquitous-mode 1))
-
-(use-package ido-vertical-mode
-  :config
-  (ido-vertical-mode 1)
-  (setq ido-vertical-define-keys 'C-n-and-C-p-only))
-
-;; vterm
-
-(use-package vterm)
-
-;; tree sitter
-
-(use-package treesit-auto
-  :init
-  (setq treesit-auto-install 'prompt)
-  :config
-  (global-treesit-auto-mode))
-
-;; Evil
-
-(use-package evil
-  :init
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
-  (setq evil-want-keybinding nil)
-  :config
-  (evil-mode 1))
-
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
-;; Magit
-(use-package magit)
-
-;; Programming languages config
-
-(add-to-list 'auto-mode-alist '("\\.tsx\\'"	.	tsx-ts-mode))
-(add-to-list 'auto-mode-alist '("\\.ts\\'"	.	typescript-ts-mode))
-
-(add-hook 'tsx-ts-mode-hook 'eglot-ensure)
-(add-hook 'typescript-ts-mode-hook 'eglot-ensure)
-
-(use-package markdown-mode)
+(use-package org)
+(org-babel-load-file
+ (expand-file-name "config.org"
+                   user-emacs-directory))
