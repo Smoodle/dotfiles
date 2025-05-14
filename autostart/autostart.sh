7ffa35eea34da1b4a8640602240c13a9d3b533df
Smoodle <migwwe@gmail.com>
2025-05-14 22:45:15 +0100

    feat: Added waybar and changed autostart

--- autostart/autostart.sh
+++ autostart/autostart.sh
@@ -2,18 +2,18 @@
 
 sleep 1
 
-wpaperd -d &
+#wpaperd -d &
 
-#(hyprpaper && (sleep 3 && $HOME/.scripts/hyprland/wallpapper.sh)) &
+hyprpaper &
 
 #gammastep -l manual -l 38.736946:-9.142685 -m wayland &
 
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 waybar &
 
 keepassxc &
 
-corectrl --minimize-systray &
+#corectrl --minimize-systray &
 
-steam &
+#steam &
